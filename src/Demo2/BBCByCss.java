23769f79b197830d4157b790e20cb674782039fb
Zhaituona <zaytuna128@gmail.com>
2018-10-17 15:35:33 -0400

    Selenium

---
+++ src/Demo2/BBCByCss.java
@@ -0,0 +1,27 @@
+package Demo2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BBCByCss {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.bbc.com");
+		driver.findElement(By.cssSelector("span[id*='username']")).click();
+		/*find Css by regular expression*/
+		driver.findElement(By.cssSelector("input[type='email']")).sendKeys("12345");
+		//find by Css use HTMl Attribute .
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("123");
+		//find Xpath use html attribute
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		//find Xpath use html attribute
+		System.out.println(driver.findElement(By.xpath("//p[contains(@class,'text')]")).getText());
+		//find Xpath use regulae expression
+		driver.navigate().back();
+		driver.close();
+	}
+
+}
---
+++ src/Demo2/RediffRegularXpath.java
@@ -0,0 +1,28 @@
+package Demo2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RediffRegularXpath {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.rediff.com");
+		driver.findElement(By.cssSelector("a[title*='Sign in']")).click();
+		//find Css regular expression
+		driver.findElement(By.xpath("//input[@name='login']")).sendKeys("hello");
+		//Find Xpath html attribute
+		//driver.findElement(By.xpath("//input[contains(@name,'login')")).sendKeys("let us check");
+		driver.findElement(By.xpath("//input[@name='passwd']")).sendKeys("123456");
+		//find Xpath html attribute
+		driver.findElement(By.xpath("//input[@value='Go']")).click();
+		//find Xpath html attribute
+		driver.navigate().back();
+		driver.close();
+		
+
+	}
+
+}
